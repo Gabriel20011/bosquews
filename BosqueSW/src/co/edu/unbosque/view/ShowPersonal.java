a4706ab44c2bbda1bbe90a1c89a2118d29e1239e
jondo <slim.shady99q@gmail.com>
2020-02-08 14:29:33 -0500

    Se añadio un logo

--- BosqueSW/src/co/edu/unbosque/view/ShowPersonal.java
+++ BosqueSW/src/co/edu/unbosque/view/ShowPersonal.java
@@ -47,91 +47,101 @@ public class ShowPersonal extends JFrame {
 
 	public void cargarInformacion(Personal personal) {
 		this.personal = personal;
+		
+		if (personal != null) {
+			txtcedula = new JLabel(personal.getCedula());
+			txtcedula.setBounds(290,15,285,25);
+			add(txtcedula);
+			
+			txtnombre = new JLabel(personal.getNombre());
+			txtnombre.setBounds(290,50,285,25);
+			add(txtnombre);
+			
+			txtapellido = new JLabel(personal.getApellido());
+			txtapellido.setBounds(290,90,285,25);
+			add(txtapellido);
+			
+			if (personal.getGenero() == 'H' || personal.getGenero() == 'h') {
+				txtgenero = new JLabel("Masculino");
+			}
+			else {
+				txtgenero = new JLabel("Femenino");
+			}
+			txtgenero.setBounds(290, 130, 285, 20);
+			add(txtgenero);
+			
+			txtcorreo = new JLabel(personal.getCorreo());
+			txtcorreo.setBounds(290, 160, 285, 20);
+			add(txtcorreo);
+			
+			txtdireccion = new JLabel(personal.getDireccion());
+			txtdireccion.setBounds(290, 190, 285, 20);
+			add(txtdireccion);
+			
+			txtanio = new JLabel(Integer.toString(personal.getAnio()));
+			txtanio.setBounds(290,220,110,19);
+			add(txtanio);
+			
+			txttelefono = new JLabel(personal.getTelefono());
+			txttelefono.setBounds(290, 260, 285, 20);
+			add(txttelefono);
+			
+			txtSalario = new JLabel(Double.toString(personal.getSalarioBase()));
+			txtSalario.setBounds(290, 300, 285, 20);
+			add(txtSalario);
+		}
+		else {
+			txtcedula = new JLabel();
+			txtnombre = new JLabel();
+			txtapellido = new JLabel();
+			txtgenero = new JLabel();
+			txtcorreo = new JLabel();
+			txtdireccion = new JLabel();
+			txtanio = new JLabel();
+			txttelefono = new JLabel();
+			txtSalario = new JLabel();
+		}
 
 		lblcedula = new JLabel("Cedula");
 		lblcedula.setBounds(45,15,155,25);
 		add(lblcedula);
 
-		txtcedula = new JLabel(personal.getCedula());
-		txtcedula.setBounds(290,15,285,25);
-		add(txtcedula);
-
 		lblnombre = new JLabel("Nombre");
 		lblnombre.setBounds(45,50,170,25);
 		add(lblnombre);
-
-		txtnombre = new JLabel(personal.getNombre());
-		txtnombre.setBounds(290,50,285,25);
-		add(txtnombre);
-
+		
 		lblapellido = new JLabel("Apellidos");
 		lblapellido.setBounds(45,90,170,25);
 		add(lblapellido);
-
-		txtapellido = new JLabel(personal.getApellido());
-		txtapellido.setBounds(290,90,285,25);
-		add(txtapellido);
-
+		
 		lblgenero = new JLabel("Genero");
 		lblgenero.setBounds(45,130,155,25);
 		add(lblgenero);
-
-		if (personal.getGenero() == 'H' || personal.getGenero() == 'h') {
-			txtgenero = new JLabel("Masculino");
-		}
-		else {
-			txtgenero = new JLabel("Femenino");
-		}
-
-		txtgenero.setBounds(290, 130, 285, 20);
-		add(txtgenero);
-
+		
 		lblcorreo = new JLabel("Correo");
 		lblcorreo.setBounds(45,160,110,20);
 		add(lblcorreo);
-
-		txtcorreo = new JLabel(personal.getCorreo());
-		txtcorreo.setBounds(290, 160, 285, 20);
-		add(txtcorreo);
 
 		lbldireccion = new JLabel("Direccion");
 		lbldireccion.setBounds(45,190,110,20);
 		add(lbldireccion);
 
-		txtdireccion = new JLabel(personal.getDireccion());
-		txtdireccion.setBounds(290, 190, 285, 20);
-		add(txtdireccion);
-
 		lblanio = new JLabel("Anio");
 		lblanio.setBounds(45,230,110,19);
 		add(lblanio);
-
-		txtanio = new JLabel(Integer.toString(personal.getAnio()));
-		txtanio.setBounds(290,220,110,19);
-		add(txtanio);
 
 		lbltelefono = new JLabel("Telefono");
 		lbltelefono.setBounds(45, 260, 285, 20);
 		add(lbltelefono);
 
-		txttelefono = new JLabel(personal.getTelefono());
-		txttelefono.setBounds(290, 260, 285, 20);
-		add(txttelefono);
-
 		lblSalario = new JLabel("Salario");
 		lblSalario.setBounds(45, 300, 285, 20);
 		add(lblSalario);
-
-		txtSalario = new JLabel(Double.toString(personal.getSalarioBase()));
-		txtSalario.setBounds(290, 300, 285, 20);
-		add(txtSalario);
 
 		menu = new JButton("Menu");
 		menu.addActionListener(principal);
 		menu.setBounds(355, 400, 200, 60);
 		add(menu);
-
-	
 	}
 
 	public JLabel getTxtnombre() {
